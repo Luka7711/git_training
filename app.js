11e0ea1dacf659ab986fd94382b96a0971774eb3
Luka7711 <35857649+Luka7711@users.noreply.github.com>
2022-11-02 17:43:08 -0500

    Added react component

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const list = [1,2,3,4,5]
 function printHello() {
     return "Hello World"
 }
+
+function Parent() {
+    return (
+        <div>
+            <h1>Title</h1>
+        </div>
+    )
+}
